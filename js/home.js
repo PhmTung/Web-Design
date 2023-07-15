e07316e473c006b32da6ec191d49c706d3e23bc3
PhmTung <139086686+PhmTung@users.noreply.github.com>
2023-07-15 23:00:37 +0700

    add file to git
    hi

---
+++ js/home.js
@@ -0,0 +1,10 @@
+document.querySelector(".TU").classList.add("show")
+
+const nShow = setTimeout(()=> {
+   document.querySelector(".N").classList.add("show")
+}, 500)
+
+const gShow = setTimeout(()=> {
+    document.querySelector(".G").classList.add("show")
+}, 1000)
+
